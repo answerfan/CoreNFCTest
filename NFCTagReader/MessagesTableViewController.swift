649c3b9cadcf2f724d5d124b692380854dcc8403
Aleksandr Boriskin <boriskin@improveitgroup.com>
2020-03-12 22:55:18 +0700

    Improve for writing

--- NFCTagReader/MessagesTableViewController.swift
+++ NFCTagReader/MessagesTableViewController.swift
@@ -1,14 +1,19 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-The view controller that scans and displays NDEF messages.
-*/
+//
+//  MessagesTableViewController.swift
+//  NFCTagReader
+//
+//  Created by Александр Борискин on 23.02.2019.
+//  Copyright © 2020 Improve Digital. All rights reserved.
+//
 
 import UIKit
 import CoreNFC
 
-/// - Tag: MessagesTableViewController
+enum NFCMode {
+    case reader
+    case writer
+}
+
 class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDelegate {
 
     // MARK: - Properties
@@ -16,11 +21,22 @@ class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDe
     let reuseIdentifier = "reuseIdentifier"
     var detectedMessages = [NFCNDEFMessage]()
     var session: NFCNDEFReaderSession?
+    var mode: NFCMode = .reader
 
     // MARK: - Actions
 
-    /// - Tag: beginScanning
+    // - Tag: beginScanning
     @IBAction func beginScanning(_ sender: Any) {
+        mode = .reader
+        startNFCSession()
+    }
+
+    @IBAction func beginWriting(_ sender: Any) {
+        mode = .writer
+        startNFCSession()
+    }
+    
+    private func startNFCSession() {
         guard NFCNDEFReaderSession.readingAvailable else {
             let alertController = UIAlertController(
                 title: "Scanning Not Supported",
@@ -32,26 +48,31 @@ class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDe
             return
         }
 
+        let message = mode == .reader ? "Read tag" : "Write test tag"
         session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
-        session?.alertMessage = "Hold your iPhone near the item to learn more about it."
+        session?.alertMessage = message
         session?.begin()
     }
-
     // MARK: - NFCNDEFReaderSessionDelegate
 
-    /// - Tag: processingTagData
+    // - Tag: processingTagData
     func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
         DispatchQueue.main.async {
-            // Process detected NFCNDEFMessage objects.
+
             self.detectedMessages.append(contentsOf: messages)
             self.tableView.reloadData()
         }
     }
 
-    /// - Tag: processingNDEFTag
+
+    // - Tag: processingNDEFTag
     func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+        
+        let message = NFCNDEFPayload.wellKnownTypeTextPayload(string: "Hello there, General Kenobi", locale: Locale.current)
+        let new = NFCNDEFPayload(format: .media, type: "UIImage".data(using: .utf8)!, identifier: "SuperUniqueIdentifier".data(using: .utf8)!, payload: UIImage(named: "ic_location")!.jpegData(compressionQuality: 1.0)!)
+        let nfcMessage = NFCNDEFMessage(records: [message!])
         if tags.count > 1 {
-            // Restart polling in 500ms
+
             let retryInterval = DispatchTimeInterval.milliseconds(500)
             session.alertMessage = "More than 1 tag is detected, please remove all tags and try again."
             DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
@@ -60,7 +81,6 @@ class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDe
             return
         }
         
-        // Connect to the found tag and perform NDEF message reading
         let tag = tags.first!
         session.connect(to: tag, completionHandler: { (error: Error?) in
             if nil != error {
@@ -68,7 +88,7 @@ class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDe
                 session.invalidate()
                 return
             }
-            
+                        
             tag.queryNDEFStatus(completionHandler: { (ndefStatus: NFCNDEFStatus, capacity: Int, error: Error?) in
                 if .notSupported == ndefStatus {
                     session.alertMessage = "Tag is not NDEF compliant"
@@ -80,39 +100,60 @@ class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDe
                     return
                 }
                 
-                tag.readNDEF(completionHandler: { (message: NFCNDEFMessage?, error: Error?) in
-                    var statusMessage: String
-                    if nil != error || nil == message {
-                        statusMessage = "Fail to read NDEF from tag"
-                    } else {
-                        statusMessage = "Found 1 NDEF message"
-                        DispatchQueue.main.async {
-                            // Process detected NFCNDEFMessage objects.
-                            self.detectedMessages.append(message!)
-                            self.tableView.reloadData()
+                if self.mode == .reader {
+                    tag.readNDEF(completionHandler: { (message: NFCNDEFMessage?, error: Error?) in
+                        var statusMessage: String
+                        if nil != error || nil == message {
+                            statusMessage = "Fail to read NDEF from tag"
+                        } else {
+                            statusMessage = "Found 1 NDEF message"
+                            if let description = message?.records.first?.wellKnownTypeTextPayload().0 {
+                                statusMessage = description
+                            }
+                            
+                            let images = message?.records.filter({ (object) -> Bool in
+                                let identifier = String(data: object.identifier, encoding: .utf8)
+                                return identifier == "SuperUniqueIdentifier"
+                            })
+                            if let imagePayload = images?.first {
+                                let image = UIImage(data: imagePayload.payload)
+                                print(image?.configuration.hashValue as Any)
+                            }
+                            DispatchQueue.main.async {
+                                
+                                self.detectedMessages.append(message!)
+                                self.tableView.reloadData()
+                            }
+                        }
+                        
+                        session.alertMessage = statusMessage
+                        session.invalidate()
+                    })
+                } else {
+                    
+                    tag.writeNDEF(nfcMessage) { error in
+
+                        if error != nil {
+                            session.invalidate(errorMessage: "Failed to write message.")
+                        } else {
+                            session.alertMessage = "Successfully wrote data to tag!"
+                            session.invalidate()
                         }
                     }
-                    
-                    session.alertMessage = statusMessage
-                    session.invalidate()
-                })
+                }
             })
         })
     }
     
-    /// - Tag: sessionBecomeActive
+    // - Tag: sessionBecomeActive
     func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
         
     }
     
-    /// - Tag: endScanning
+    // - Tag: endScanning
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
-        // Check the invalidation reason from the returned error.
         if let readerError = error as? NFCReaderError {
-            // Show an alert when the invalidation reason is not because of a
-            // successful read during a single-tag read session, or because the
-            // user canceled a multiple-tag read session from the UI or
-            // programmatically using the invalidate method call.
+
             if (readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)
                 && (readerError.code != .readerSessionInvalidationErrorUserCanceled) {
                 let alertController = UIAlertController(
@@ -127,12 +168,11 @@ class MessagesTableViewController: UITableViewController, NFCNDEFReaderSessionDe
             }
         }
 
-        // To read new tags, a new session instance is required.
         self.session = nil
     }
 
-    // MARK: - addMessage(fromUserActivity:)
 
+    // MARK: - addMessage(fromUserActivity:)
     func addMessage(fromUserActivity message: NFCNDEFMessage) {
         DispatchQueue.main.async {
             self.detectedMessages.append(message)
