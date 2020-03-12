06645863c56bd92793ee20b20120f15d9fd9fd09
Aleksandr Boriskin <boriskin@improveitgroup.com>
2020-03-12 23:31:57 +0700

    add background reader support

--- NFCTagReader/AppDelegate.swift
+++ NFCTagReader/AppDelegate.swift
@@ -6,6 +6,7 @@ Main application delegate
 */
 
 import UIKit
+import CoreNFC
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -36,4 +37,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    
+    func application(_ application: UIApplication,
+                     continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb else {
+            return false
+        }
+
+        // Confirm that the NSUserActivity object contains a valid NDEF message.
+        let ndefMessage = userActivity.ndefMessagePayload
+        guard ndefMessage.records.count > 0,
+            ndefMessage.records[0].typeNameFormat != .empty else {
+                return false
+        }
+
+        // Send the message to `MessagesTableViewController` for processing.
+        guard let navigationController = window?.rootViewController as? UINavigationController else {
+            return false
+        }
+
+        navigationController.popToRootViewController(animated: true)
+        let messageTableViewController = navigationController.topViewController as? MessagesTableViewController
+        messageTableViewController?.addMessage(fromUserActivity: ndefMessage)
+
+        return true
+    }
+
 }
